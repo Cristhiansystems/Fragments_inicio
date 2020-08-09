96377b846c1e574ef1d3e656d01e1a628dd23908
Cristhiansystems <69399024+Cristhiansystems@users.noreply.github.com>
2020-08-09 17:28:33 -0400

    2.Como usar fragments dinamicos

--- app/src/main/java/com/example/fragments_inicio/MainActivity.java
+++ app/src/main/java/com/example/fragments_inicio/MainActivity.java
@@ -1,20 +1,49 @@
 package com.example.fragments_inicio;
 
 import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements  fragment_rojo.OnFragmentInteractionListener,
-        fragment_verde.OnFragmentInteractionListener{
+        fragment_verde.OnFragmentInteractionListener,fragment_azul.OnFragmentInteractionListener{
 
+    fragment_rojo Fragment_rojo;
+    fragment_verde Fragment_verde;
+    fragment_azul Fragment_azul;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fragment_azul=new fragment_azul();
+        Fragment_rojo=new fragment_rojo();
+        Fragment_verde=new fragment_verde();
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragments,Fragment_rojo).commit();
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    public void onClick(View view){
+
+        FragmentTransaction Transaction=getSupportFragmentManager().beginTransaction();
+        switch (view.getId()) {
+            case    R.id.btn1:
+                Transaction.replace(R.id.contenedorFragments,Fragment_rojo);
+                break;
+            case R.id.btn2:
+                Transaction.replace(R.id.contenedorFragments,Fragment_verde);
+                break;
+            case R.id.btn3:
+                Transaction.replace(R.id.contenedorFragments,Fragment_azul);
+                break;
+
+        }
+
+        Transaction.commit();
     }
 }
